30ebff213e7f97f1581943e36d6c0be4b29670c7
Christoffer Nilsson <christoffer.nilsson@svenskaspel.se>
2026-03-04 20:58:13 +0100

    small changes

--- keyboards/ferris/keymaps/christnil/keymap.c
+++ keyboards/ferris/keymaps/christnil/keymap.c
@@ -29,13 +29,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [SYMBOLS_LAYER] = LAYOUT(
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
   KC_ESCAPE,      KC_AT,          KC_HASH,        KC_DLR,          KC_PERC,
-                                                                                  KC_CIRC, KC_AMPR,         KC_ASTR,         KC_SCLN,         KC_BSPC,
+                                                                                  KC_CIRC, KC_AMPR, KC_ASTR, KC_SCLN, KC_BSPC,
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
   LSFT_T(KC_TAB), LCTL_T(KC_EQL), LALT_T(KC_GRV), LGUI_T(KC_QUOT), KC_DQUO,
-                                                                                  KC_BSLS, RGUI_T(KC_LCBR), RALT_T(KC_RCBR), RCTL_T(KC_PIPE), RSFT_T(KC_ENT),
+                                                                                  KC_BSLS, KC_LCBR, KC_RCBR, KC_PIPE, RSFT_T(KC_ENT),
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
   KC_TILD,        KC_EXLM,        KC_QUES,        KC_UNDS,         KC_MINS,
-                                                                                  KC_LBRC, KC_LPRN,         KC_RPRN,         KC_RBRC,         TO(NAVIGATION_LAYER),
+                                                                                  KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, TO(NAVIGATION_LAYER),
   //---------------------------------------------------------------------------------------       ----------------------------------------------------------------------------------
                                                   TO(COLEMAK_DH),  OSM(MOD_LSFT),
                                                                                   KC_RALT, TO(MEDIA_LAYER) 
